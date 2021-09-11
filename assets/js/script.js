dd91efb9b236284b551ab2640fc669f1741a5170
Chris Zuniga <zunigachristian81@gmail.com>
2021-09-10 22:40:33 -0500

    password requirements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,51 @@
 // Assignment code here
+var long;
+var confirmNum;
+var confirmSymbol;
+var confirmUpper;
+var confirmLower;
+
+
+
+
+upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+num = [0,1,2,3,4,5,6,7,8,9];
+symbol = ["!","@","#","$","%","&","?"]
+
+
+
+function generatePassword() {
+  long = parseInt(prompt("How many characters would you like your password? [8-128]"));
+    
+  if (long > 7 && long < 129 ) {
+    console.log(long);
+    confirmNum = confirm("Include numbers?");
+    confirmSymbol = confirm("Include special characters?");
+    confirmUpper = confirm("Include Uppercase letters?");
+    confirmLower = confirm("Include Lowercase letters?");
+  }
+
+  else {
+    alert("Provide a correct value!")
+  }
+   return generatePassword();
+
+};
+
+
+
+
+
+
+
+
+
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
+
 
 // Write password to the #password input
 function writePassword() {
@@ -14,4 +57,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
+
